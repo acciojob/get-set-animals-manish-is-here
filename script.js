8f1bdeee428c6fdba33ab233a39e0358867b3db9
manish-is-here <151768806+manish-is-here@users.noreply.github.com>
2024-05-16 21:37:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
-//complete this code
-class Animal {}
+class Animal {
+    constructor(species) {
+        this.species = species;
+    }
 
-class Dog extends Animal {}
+    getSpecies() {
+        return this.species;
+    }
 
-class Cat extends Animal {}
+    makeSound() {
+        console.log("The animal makes a sound.");
+    }
+}
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+class Cat extends Animal {
+    constructor(species) {
+        super(species);
+    }
+
+    purr() {
+        console.log("purr");
+    }
+
+    makeSound() {
+        super.makeSound();
+        console.log("The cat says 'meow'.");
+    }
+}
+
+class Dog extends Animal {
+    constructor(species) {
+        super(species);
+    }
+
+    bark() {
+        console.log("woof");
+    }
+
+    makeSound() {
+        super.makeSound();
+        console.log("The dog says 'woof'.");
+    }
+}
+
+const myCat = new Cat("domestic cat");
+const myDog = new Dog("German Shepherd");
+
+document.getElementById("cat-btn").addEventListener("click", () => {
+    myCat.makeSound();
+    myCat.purr();
+});
+
+document.getElementById("dog-btn").addEventListener("click", () => {
+    myDog.makeSound();
+    myDog.bark();
+});
